733e1fe2021b2224c623ae86ce90a280d722fdc4
Carlos Daniel <168373952+Zarconiel@users.noreply.github.com>
2025-10-29 18:58:17 -0300

    frontend
    Foi feito a instalação do react com tailwindcss em uma pasta para o frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+      <h1>
+        Ola mundo!
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
